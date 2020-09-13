9db64e233a11e1065a11390cb72c36d72af4aac3
Abhishek Kumar <abhishekkumarjnvk@gmail.com>
2020-09-13 22:57:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    scanner: null,
+    activeCameraId: null,
+    cameras: [],
+    scans: []
+  },
+  mounted: function () {
+    var self = this;
+    self.scanner = new Instascan.Scanner({
+      video: document.getElementById('preview'),
+      scanPeriod: 5
+    });
+    self.scanner.addListener('scan', function (content, image) {
+      self.scans.unshift({
+        date: +(Date.now()),
+        content: content
+      });
+      $("#scan_history").prepend(`<li  class='list-group-item border border-secondary mt-2' >  ${content}</li>`);
+
+      $("#server_message").text('QR Code Scanned');
+      $("#server_response").css('background-color', 'green');
+      $("#server_response").show();
+
+      setTimeout(() => {
+        $("#server_response").hide();
+      }, 2000);
+
+      console.log(content);
+    });
+
+
+    Instascan.Camera.getCameras().then(function (cameras) {
+      self.cameras = cameras;
+      if (cameras.length > 0) {
+        self.activeCameraId = cameras[0].id;
+        self.scanner.start(cameras[0]);
+      } else {
+        console.error('No cameras found.');
+      }
+    }).catch(function (e) {
+      console.error(e);
+    });
+  },
+  methods: {
+    formatName: function (name) {
+      return name || '(unknown)';
+    },
+    selectCamera: function (camera) {
+      this.activeCameraId = camera.id;
+      this.scanner.start(camera);
+    }
+  }
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# QR Scanner
+its a simple & light weight realtime QR Code Scanner. Built with javascript.
+##### Features
+    * Realtime Scanning
+    * Previous Scans
+
+### Library Used
+    * Bootstrap
+    * Jquery
+    * Font Awesome
+    * insta Scan
